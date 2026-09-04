2d548840ca0b4e0bbd60605e5a73d7016087af75
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-04 04:26:48 -0700

    fix: remove obsolete stylesheet import from router app

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,17 @@ import Checkout from './pages/Checkout'
 import Login from './pages/Login'
 import About from './pages/About'
 import Contact from './pages/Contact'
-import './multipage.css'
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Layout cartCount={0}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/product/:id" element={<ProductDetails />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
-  )
+  return <BrowserRouter><Layout cartCount={0}><Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/shop" element={<Shop />} />
+    <Route path="/product/:id" element={<ProductDetails />} />
+    <Route path="/cart" element={<Cart />} />
+    <Route path="/checkout" element={<Checkout />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="*" element={<Home />} />
+  </Routes></Layout></BrowserRouter>
 }
