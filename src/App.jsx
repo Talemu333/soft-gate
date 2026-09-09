d4ec3e88bbdc756254a8ce7998dc37e8c96872a9
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-09 00:16:22 -0700

    feat: add professional ecommerce app shell

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { CartProvider } from './context/CartContext'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import Shop from './pages/Shop'
@@ -10,15 +11,23 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 
 export default function App() {
-  return <BrowserRouter><Layout cartCount={0}><Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/shop" element={<Shop />} />
-    <Route path="/product/:id" element={<ProductDetails />} />
-    <Route path="/cart" element={<Cart />} />
-    <Route path="/checkout" element={<Checkout />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/contact" element={<Contact />} />
-    <Route path="*" element={<Home />} />
-  </Routes></Layout></BrowserRouter>
+  return (
+    <BrowserRouter>
+      <CartProvider>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </Layout>
+      </CartProvider>
+    </BrowserRouter>
+  )
 }
